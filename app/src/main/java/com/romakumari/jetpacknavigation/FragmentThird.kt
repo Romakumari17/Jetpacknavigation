dfda085a1efc190b2df40561ec13a44580575f39
Romakumari17 <138012027+Romakumari17@users.noreply.github.com>
2024-03-22 20:47:32 +0530

    Initial commit

--- app/src/main/java/com/romakumari/jetpacknavigation/FragmentThird.kt
+++ app/src/main/java/com/romakumari/jetpacknavigation/FragmentThird.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
